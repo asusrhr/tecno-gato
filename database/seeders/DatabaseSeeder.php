d571a3dd5e7441a002eb78d6b7ccaf83e6d44532
Ronaldo Rivero Gonzales <ronaldorivero3@gmail.com>
2021-03-28 21:04:18 -0400

    agregando seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Administrador;
+use App\Models\Persona;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $persona = new Persona();
+        $persona->nombre = 'Admin';
+        $persona->apellido_paterno = 'Admin';
+        $persona->apellido_materno = 'Admin';
+        $persona->carnet_identidad = '000000000';
+        $persona->direccion = '2do anillo av mutualista';
+        $persona->fecha_nacimiento = '01/01/2021';
+        $persona->telefono = '000000000';
+        $persona->save();
+
+        $administrador = new Administrador();
+        $administrador->id_persona = $persona->id;
+        $administrador->save();
+
+        $user = new User();
+        $user->name= 'adminadmin';
+        $user->email= 'admin@admin.com';
+        $user->password = bcrypt('1234');
+        $user->id_persona = $persona->id;
+        $user->save();
     }
 }
